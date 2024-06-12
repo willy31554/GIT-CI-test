f1dc76232871ddd4edba9c39d0fd41db0efa0eea
unknown <marknagtia73@gmail.com>
2024-06-12 10:18:28 +0300

    Brief description of the changes66

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -10,7 +10,7 @@ import my_module
 
 # Define test functions using pytest
 def test_addition():
-    assert my_module.add(1, 2) == 3
+    assert my_module.add(1, 2) == 4
 
 def test_subtraction():
     assert my_module.subtract(5, 3) == 2
